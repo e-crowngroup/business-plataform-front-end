0189aeaa488b45632e8618738da093c856e0a320
E-crown <87323964+e-crowngroup@users.noreply.github.com>
2021-07-30 17:22:47 -0300

    business-plataform-front-end
    Plataform B.i, front login.
    
    Usuário principal:  admin, senha: admin .
    Usuário Dashboard: dash senha: admin

---
+++ precache-manifest.7abc200ffb488e712abb263bc6248392.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "294741840c775d5083325006167e4874",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c85ea3239fc1802f89b",
+    "url": "/static/css/main.5719d698.chunk.css"
+  },
+  {
+    "revision": "968fc20ecab578b42df8",
+    "url": "/static/js/2.f2d8d4c2.chunk.js"
+  },
+  {
+    "revision": "b76cd0b7294280e24e2077be230640d3",
+    "url": "/static/js/2.f2d8d4c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c85ea3239fc1802f89b",
+    "url": "/static/js/main.25280948.chunk.js"
+  },
+  {
+    "revision": "c5f758ab953c6d6f2987",
+    "url": "/static/js/runtime-main.63661cca.js"
+  }
+]);
